36e3c008378e61d6f9b7b766e9d792f536c0d139
seitamuro <seitamuro0210@gmail.com>
2021-07-12 19:40:27 +0900

    add abc206 a b c

---
+++ abc206/src/bin/c.rs
@@ -0,0 +1,266 @@
+#![allow(dead_code, non_snake_case, unused_imports)]
+use proconio::input;
+use std::cmp::Ordering;
+use itertools;
+use std::collections::HashMap;
+
+fn main() {
+    input! {
+        n: usize,
+        a: [usize; n]
+    }
+
+    let mut hash = HashMap::new();
+    let mut total = 0usize;
+    for i in a.iter() {
+        hash.insert(i, *hash.get(i).unwrap_or(&0) + 1);
+        total += 1;
+    }
+
+    let mut ans = 0;
+    for i in a.iter() {
+        ans += total - hash.get(i).unwrap_or(&0);
+        total -= 1;
+        hash.insert(i, hash.get(i).unwrap_or(&0) - 1);
+    }
+
+    println!("{}", ans);
+}
+
+/// path: path to the other nodes
+/// cost: weight of the path corresponding to each path
+/// https://qiita.com/okaryo/items/8e6cd73f8a676b7a5d75
+fn warshall_floyd(path: Vec<Vec<usize>>, cost: Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    let mut table = vec![vec![std::usize::MAX; path.len()]; path.len()];
+
+    for i in 0..path.len() {
+        table[i][i] = 0;
+        for (j, c) in path[i].iter().zip(cost[i].iter()) {
+            table[i][*j] = *c;
+        }
+    }
+
+    for k in 0..path.len() {
+        for i in 0..path.len() {
+            for j in 0..path.len() {
+                println!("{} -> {} | {} | {} {}", i, j, table[i][j], table[i][k], table[k][j]);
+                table[i][j] = table[i][j].min(table[i][k].saturating_add(table[k][j]));
+            }
+        }
+    }
+
+    table
+}
+
+fn y() {
+    println!("yes");
+}
+
+fn Y() {
+    println!("Yes");
+}
+
+fn n() {
+    println!("no");
+}
+
+fn N() {
+    println!("No");
+}
+
+#[macro_export]
+macro_rules! ans {
+    ($i:item) => {
+        println!("{}", stringify!($i))
+    };
+}
+
+/// get_divisors function returns the list of divisors of n.
+fn get_divisors(n: usize) -> Vec<usize> {
+    // https://algo-logic.info/divisor/
+    let nsq = (n as f32).sqrt() as usize;
+    let mut ans1 = Vec::new();
+    let mut ans2 = Vec::new();
+
+    for i in 1..nsq {
+        if n % i == 0 {
+            ans1.push(i);
+            ans2.insert(0, n / i);
+        }
+    }
+
+    if n % nsq == 0 {
+        ans1.push(nsq);
+
+        if n / nsq != nsq {
+            ans1.push(n / nsq);
+        }
+    }
+
+    ans1.append(&mut ans2);
+    ans1
+}
+
+/// gcd function returns the greatest common factor of a and b.
+/// For example, gcd(12, 7) returns 1.
+fn gcd(a: usize, b: usize) -> usize {
+    let a = a % b;
+
+    if a == 0 {
+        return b;
+    }
+
+    let greater = a.max(b);
+    let smaller = a.min(b);
+
+    gcd(greater, smaller)
+}
+
+/// lcm function returns the least common multiple of a and b.
+/// For exmaple, gcd(4, 6) returns 24.
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
+}
+
+fn search<T: Ord>(key: T, elems: &[T]) -> Option<usize> {
+    let mut low = 0;
+    let mut high = elems.len();
+
+    while low != high {
+        let m = (high - low) / 2 + low;
+
+        match elems[m].cmp(&key) {
+            Ordering::Equal => return Some(m),
+            Ordering::Less => {
+                if m == elems.len() - 1 {
+                    return None;
+                }
+
+                low = m + 1;
+            }
+            Ordering::Greater => {
+                if m == 0 {
+                    return None;
+                }
+
+                high = m - 1;
+            }
+        }
+    }
+
+    if elems[low] == key {
+        Some(low)
+    } else {
+        None
+    }
+}
+
+/// 幅優先探索
+/// startからの各ノードへの最短距離を返す
+/// path[i]はnode[i]が移動可能なノードを表す｡
+fn bfs(start: usize, node: &[usize], path: &[Vec<usize>]) -> Vec<Option<usize>> {
+    let mut ans = vec![None; node.len()];
+    let mut todo = Vec::<usize>::new();
+
+    ans[start] = Some(0);
+    for &i in path[start].iter() {
+        if ans[i] == None {
+            let x = ans[start].unwrap();
+            todo.push(i);
+            ans[i] = Some(x + 1);
+        }
+    }
+
+    while !todo.is_empty() {
+        let i = todo.remove(0);
+
+        for &node in path[i].iter() {
+            if ans[node] == None {
+                let x = ans[i].unwrap();
+                todo.push(node);
+                ans[node] = Some(x + 1);
+            }
+        }
+    }
+
+    ans
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_gcd() {
+        assert_eq!(gcd(2, 2), 2);
+        assert_eq!(gcd(2, 6), 2);
+        assert_eq!(gcd(12, 16), 4);
+        assert_eq!(gcd(16, 12), 4);
+        assert_eq!(gcd(123, 456), 3);
+    }
+
+    #[test]
+    fn test_lcm() {
+        assert_eq!(lcm(2, 2), 2);
+        assert_eq!(lcm(2, 4), 4);
+        assert_eq!(lcm(12, 10), 60);
+        assert_eq!(lcm(6, 7), 42);
+        assert_eq!(lcm(123, 456), 18696);
+    }
+
+    #[test]
+    fn test_search() {
+        let a = [1, 3, 6, 8, 12, 20];
+        for i in 0..a.len() {
+            assert_eq!(search(a[i], &a), Some(i));
+        }
+
+        assert_eq!(search(0, &a), None);
+        assert_eq!(search(21, &a), None);
+        assert_eq!(search(2, &a), None);
+        assert_eq!(search(9, &a), None);
+
+        let a = [1, 3, 6, 8, 12];
+        for i in 0..a.len() {
+            assert_eq!(search(a[i], &a), Some(i));
+        }
+
+        assert_eq!(search(0, &a), None);
+        assert_eq!(search(21, &a), None);
+        assert_eq!(search(2, &a), None);
+        assert_eq!(search(9, &a), None);
+    }
+
+    #[test]
+    fn test_bfs() {
+        let a = vec![0, 1, 2, 3];
+        let path = vec![vec![1, 2], vec![0, 3], vec![0, 3], vec![1, 2]];
+
+        assert_eq!(bfs(0, &a, &path), vec![Some(0), Some(1), Some(1), Some(2)]);
+    }
+
+    #[test]
+    fn test_get_divisors() {
+        assert_eq!(get_divisors(36), vec![1, 2, 3, 4, 6, 9, 12, 18, 36]);
+        assert_eq!(get_divisors(24), vec![1, 2, 3, 4, 6, 8, 12, 24]);
+        assert_eq!(get_divisors(1), vec![1]);
+    }
+
+    #[test]
+    fn test_warshall_floyd() {
+        let path = vec![vec![1, 2, 3], vec![0, 3], vec![0, 3], vec![0, 1, 2]];
+
+        let cost = vec![vec![5, 3, 8], vec![5, 2], vec![3, 20], vec![8, 2, 20]];
+
+        let ans = vec![
+            vec![0, 5, 3, 7],
+            vec![5, 0, 8, 2],
+            vec![3, 8, 0, 10],
+            vec![7, 2, 10, 0],
+        ];
+
+        let ret = warshall_floyd(path, cost);
+
+        assert_eq!(ret, ans);
+    }
+}
