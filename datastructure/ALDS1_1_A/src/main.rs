a8e98e62d99d735fca0b2985e5b6efe0f94ca9ee
seitamuro <seitamuro0210@gmail.com>
2021-09-10 23:25:36 +0900

    add ALDS1_1_A

---
+++ datastructure/ALDS1_1_A/src/main.rs
@@ -0,0 +1,31 @@
+use std::io::Read;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+    let iter = buf.split_whitespace();
+    let mut numbers: Vec<usize> = Vec::new();
+    for i in iter.skip(1) {
+        numbers.push(i.parse().unwrap());
+    }
+
+    for i in 0..numbers.len() {
+        for j in 0..i {
+            if numbers[j] > numbers[i] {
+                let mut tmp1 = numbers[i];
+                let mut tmp2;
+                for k in j..=i {
+                    tmp2 = numbers[k];
+                    numbers[k] = tmp1;
+                    tmp1 = tmp2;
+                }
+                break;
+            }
+        }
+
+        for j in numbers.iter() {
+            print!("{} ", j);
+        }
+        println!("");
+    }
+}
