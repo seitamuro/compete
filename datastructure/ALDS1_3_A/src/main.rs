5d05203256f6a004e86933eb2668bdf7bff24e8f
seitamuro <seitamuro0210@gmail.com>
2021-09-15 16:09:35 +0900

    add ALDS1_3_A

--- datastructure/ALDS1_3_A/src/main.rs
+++ datastructure/ALDS1_3_A/src/main.rs
@@ -4,9 +4,26 @@ fn main() {
     let mut buf = String::new();
     std::io::stdin().read_to_string(&mut buf).unwrap();
     let iter = buf.split_whitespace();
+    let mut stack: Stack<String> = Stack::new();
+    //let iter = "1 2 + 3 4 - *".split(" ");
 
     for i in iter {
+        if "+-*/".contains(i) {
+            let num2: i32 = stack.pop().unwrap().parse().unwrap();
+            let num1: i32 = stack.pop().unwrap().parse().unwrap();
+            stack.push(match i {
+                "+" => format!("{}", num1 + num2),
+                "-" => format!("{}", num1 - num2),
+                "*" => format!("{}", num1 * num2),
+                "/" => format!("{}", num1 / num2),
+                _  => panic!("Unexpected operand!"),
+            });
+        } else {
+            stack.push(i.to_string());
+        }
     }
+
+    println!("{}", stack.pop().unwrap());
 }
 
 struct Stack<T> {
@@ -28,6 +45,11 @@ impl<T> Stack<T> {
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        self.length -= 1;
+        if self.length > 0 {
+            self.length -=1;
+            self.data.pop()
+        } else {
+            None
+        }
     }
 }
